c7cb998b93a740aeabbfd781b5e7828a0497b66c
AnnaGudkova <anyagud@rambler.ru>
2014-03-12 16:13:17 +0600

    Create MyWoodLoaderTest.java

---
+++ MyWoodLoaderTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.junit.Test;
+
+public class MyWoodLoaderTest {
+
+	@Test
+	public void testLoad() throws IOException, CodeException {
+		File file=new File("world.txt");
+		FileInputStream stream=new FileInputStream(file);
+		MyWoodLoader W =new MyWoodLoader();	
+		Wood wood=W.Load(stream);
+		Point k=new Point(1,1);
+		wood.createWoodman("A", k);
+		assertEquals(wood.move("A",Direction.Down) , Action.Fail);
+		}
+}
