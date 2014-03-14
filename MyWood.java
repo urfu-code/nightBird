f96c86998c3ddec9d32f23419b0a9cc87e085138
Anna <annagudkova94@gmail.com>
2014-03-14 15:14:28 +0600

    Update MyWood.java

--- MyWood.java
+++ MyWood.java
@@ -3,34 +3,30 @@ import java.util.Map;
 
 public class MyWood implements Wood {
 	Map <String,MyWoodman> m_woodmanList=new HashMap<String,MyWoodman>();
-	protected char [][] m_wood;
+	private char [][] m_wood;
 
 	public MyWood(char[][] wood) {
 		m_wood=wood;
 	}
 
-	public void createWoodman(String name, Point start) {
-		if (!m_woodmanList.containsKey(name)){
+	public void createWoodman(String name, Point start) throws CodeException {
 		MyWoodman m_woodman=new MyWoodman(name,start);
+		if (!m_woodmanList.containsKey(name)) 
 		m_woodmanList.put(name,m_woodman);		
-	}
+		else throw new CodeException("This woodman exists!");
 	}
 
-	public Action move(String name, Direction direction) throws CodeException {
+	public Action move(String name, Direction direction)  {
 		Action result=null;
 		if (!m_woodmanList.containsKey(name)) {
 			result=Action.WoodmanNotFound;
 		} else {
 			Point n=m_woodmanList.get(name).GetLocation();
-			if(m_woodmanList.get(name).GetLifeCount()>-1) {
+	
 				int x=0; int y=0;		
 				int i=m_woodmanList.get(name).GetLocation().getX();
-				if (i>m_wood.length||i<0) 
-					throw new CodeException("Input correct coordinates!");
 				int j=m_woodmanList.get(name).GetLocation().getY();
-				if (j>m_wood.length||j<0) 
-					throw new CodeException("Input correct coordinates!");		
-				if (direction==Direction.Up) { 
+					if (direction==Direction.Up) { 
 					n=new Point(i-1,j);
 					x=i-1; y=j;
 				}
@@ -52,14 +48,19 @@ public class MyWood implements Wood {
 				if (m_wood[x][y]=='K') {
 					m_woodmanList.get(name).Kill();
 					result=Action.Dead;
-					m_woodmanList.get(name).SetLocation(n);
-					if(m_woodmanList.get(name).GetLifeCount()<=-1){
-						m_woodmanList.remove(name);
-						result=Action.WoodmanNotFound;
-					}
 				}
 				if (m_wood[x][y]=='1') {
-					result=Action.Fail;
+					if(m_wood[i][j]=='K') 
+						{
+						m_woodmanList.get(name).Kill();
+						result=Action.Dead;
+						}
+					if(m_wood[i][j]=='L'){
+						m_woodmanList.get(name).AddLife();
+						result=Action.Life;
+					}
+					else 
+						result=Action.Fail;
 				}
 				if (m_wood[x][y]=='0') {
 					result=Action.Ok;
@@ -68,13 +69,12 @@ public class MyWood implements Wood {
 				if (m_wood[x][y]=='L') {
 					m_woodmanList.get(name).AddLife();
 					result=Action.Life; 
-					m_woodmanList.get(name).SetLocation(n);
 				}
-			} else {
+				if(m_woodmanList.get(name).GetLifeCount()<=-1) {
 				m_woodmanList.remove(name);
 				result=Action.WoodmanNotFound;
+				}
 			}
-		}
 		return result;
 	}
 }
